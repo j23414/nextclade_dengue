d35113cbe36b85c9bde3922668fba7428aa0a6c3
j23414 <jennifer.chang.bioinform@gmail.com>
2023-04-24 11:09:42 -0700

    Changes for Dengue dataset
    Regenerate the tree.json using the example sequences and metadata listed at:
    
    * https://github.com/nextstrain/dengue/tree/main/example_data
    
    The tree.json was generated using attached script.sh. The header of the tree
    was modified using the attached auspice_config.json file.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # nextclade_dengue
+
 Attempt to create a nextclade_dengue dataset
+
+## Potential References
+
+Identify potential references based on files listed in the config folder.
+
+* https://github.com/nextstrain/dengue/tree/main/config
+
+| clade_membership | accession | strain |
+|:--|:--|:--|
+| | NC_002640 | DENV4/NA/REFERENCE/2003 (DENV4)|
+| DENV1 | NC_001477 | DENV1/NAURUISLAND/REFERENCE/1997 |
+| DENV2 | NC_001474 | DENV2/THAILAND/REFERENCE/1964 |
+| DENV3 | NC_001475 | DENV3/SRI_LANKA/REFERENCE/2000 |
+| DENV4 | NC_002640 | DENV4/NA/REFERENCE/2003 |
---
+++ script.sh
@@ -0,0 +1,70 @@
+#! /usr/bin/env bash
+set -euv
+
+INFILE="sequences.fasta"
+META="metadata.tsv"
+REF="reference.fasta"
+REF_GFF="reference_dengue_all.gb"
+
+[[ -d "results" ]] || mkdir results
+
+# echo "strain|date|clade_membership" | tr '|' '\t' > metadata.tsv
+# grep ">" ${INFILE} \
+#   | sed 's/>//g' \
+#   | awk -F'|' '{print $0"\t"$3"\t"$4}' >> metadata.tsv
+
+augur align \
+  --sequences ${INFILE} \
+  --reference-sequence ${REF} \
+  --output results/prrsv_aln.fasta \
+  --fill-gaps \
+  --nthreads 1
+
+augur tree \
+  --alignment results/prrsv_aln.fasta \
+  --output results/tree.nwk \
+  --nthreads 1
+
+augur refine \
+  --tree results/tree.nwk \
+  --alignment results/prrsv_aln.fasta \
+  --metadata metadata.tsv \
+  --output-tree results/refined_tree.nwk \
+  --output-node-data results/branch_labels.json
+
+augur ancestral \
+  --tree results/refined_tree.nwk \
+  --alignment results/prrsv_aln.fasta \
+  --output-node-data results/nt-muts.json \
+  --inference joint
+
+augur translate \
+  --tree results/refined_tree.nwk \
+  --ancestral-sequences results/nt-muts.json \
+  --reference-sequence ${REF_GFF} \
+  --output results/aa-muts.json
+
+augur traits \
+  --tree results/refined_tree.nwk \
+  --metadata metadata.tsv \
+  --output results/clade_membership.json \
+  --columns clade_membership 
+
+augur export v2 \
+  --tree results/refined_tree.nwk \
+  --node-data \
+    results/branch_labels.json \
+    results/clade_membership.json \
+    results/nt-muts.json \
+    results/aa-muts.json \
+  --output results/tree.json \
+  --auspice-config auspice_config.json
+
+echo "Done! look at results/tree.json"
+
+# zip into dengue.zip
+# nextclade run -D dengue.zip -j 4 \
+#   --output-fasta output_alignment.fasta \
+#   --output-translations translations_{gene}.zip \
+#   --output-insertions insertions.csv \
+#   sequences.fasta
